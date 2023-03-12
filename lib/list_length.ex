2e726ea12f05abfb065ee31e372ee424f4f1bbd0
Thiago Freire <thiago.freire45@gmail.com>
2023-03-12 02:51:34 -0300

    finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    get_length(list,0)
+  end
+
+  def get_length([], acc), do: acc
+
+  def get_length([head | tail], acc) do
+    acc = acc + 1
+    get_length(tail,acc)
   end
 end
